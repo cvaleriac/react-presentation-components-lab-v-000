bb40f717cbdf9658401be86a4ca243cc0833a354
valeria campilongo <cvaleriac@hotmail.it>
2019-10-14 14:01:12 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,14 @@
 // Code SimpleComponent Here
+const state = "Happy"
+ 
+class TextField extends Component {
+  render() {
+    return (
+      <input
+        className="field field-light"
+        onChange={this.props.onChange}
+        defaultstate={this.props.default || defaultstate}
+      />
+    );
+  }
+}
